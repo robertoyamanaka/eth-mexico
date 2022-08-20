2e76bcbec8582245d0fc9666c41f0fc68e8734be
GerGodfrey <luis-godfrey@hotmail.com>
2022-08-19 21:53:21 -0500

    creaciÃon de carpetas

---
+++ utils/resources.py
@@ -0,0 +1,22 @@
+#Keys 
+
+KEYS_T = { "username": "@godfreygerardo",
+        "App name": "DD_twitter",
+        "api_key": "uyrosEl3ciPmmlejtis8iCT4v",
+        "api_key_secret": "YziAGhRhLsZH9ac6w8IHAbT4OGcZ3k9mLRmHwv4YWVMeYQNfwo",
+        "bearer_token": "AAAAAAAAAAAAAAAAAAAAAEY2dwEAAAAAmhjDjlApXDFKJgC6YuMdNNVx6HA%3DrtWR8YGg8fYf4J8TLDFv19Cy06AMT6bLxtM0ED9Wzo0TPsr064",
+        "access_token": "1143967147951042560-fqc8wZJwhA5KzJd9YCLpF1ZVZpF9Vm",
+        "acces_token_secret": "VZIKYBJUZ57tEow5aMHx13HI6IQKCEETSN72hTWXyidr0"
+    }
+
+KEYS_E = "UMAGCB1RZJRTJDGS4B4K58TYAV29G94KJ9"
+
+#Etherscan actions 
+
+BALANCE = {"url" : "https://api.etherscan.io/api",
+    "module" : "?module=account",
+    "action" : "&action=balance",
+    "address" : "&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae",
+    "tag" : "&tag=latest",
+    "apiK" : "&apikey=YourApiKeyToken"
+}
